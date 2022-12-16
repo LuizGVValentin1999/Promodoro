2cabbc01871ed5ac5f0ea28929dd603e75f29dba
LuizGVValentin1999 <59962559+LuizGVValentin1999@users.noreply.github.com>
2022-12-15 22:04:50 -0300

    instalando styled-components

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,16 @@
 import { useState } from 'react'
+import { Button } from './components/Button'
 
 export function App() {
   const [count, setCount] = useState(0)
 
   return (
-   <h1>Promodoro </h1>
+  <>
+    <Button variant="primary" />
+    <Button variant="secondary" />
+    <Button variant="success" />
+    <Button variant="danger" />
+    <Button />
+  </>
   )
 }
