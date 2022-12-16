a2de9ead64406e507aae53634ea002f65205a0a7
LuizGVValentin1999 <59962559+LuizGVValentin1999@users.noreply.github.com>
2022-12-15 22:50:03 -0300

    colocando  eslintrc no projeto

--- src/@types/styled.d.ts
+++ src/@types/styled.d.ts
@@ -1,10 +1,10 @@
-import 'styled-components';
-import { defaultTheme } from '../styles/themes/default';
+import 'styled-components'
+import { defaultTheme } from '../styles/themes/default'
 
-//criando os types do projeto com base no meu tema default
-type ThemeType = typeof defaultTheme;
+// criando os types do projeto com base no meu tema default
+type ThemeType = typeof defaultTheme
 
-//definido esse tyoes para o projeto
+// definido esse tyoes para o projeto
 declare module 'styled-components' {
   export interface DefaultTheme extends ThemeType {}
 }
--- src/App.tsx
+++ src/App.tsx
@@ -1,21 +1,18 @@
-import { ThemeProvider } from "styled-components";
-import { useState } from 'react'
+import { ThemeProvider } from 'styled-components'
 import { Button } from './components/Button'
 
-import { defaultTheme } from "./styles/themes/default";
-import { GlobalStyle } from "./styles/global";
+import { defaultTheme } from './styles/themes/default'
+import { GlobalStyle } from './styles/global'
 
 export function App() {
-  const [count, setCount] = useState(0)
-
   return (
-  <ThemeProvider theme={defaultTheme}>
-    <Button variant="primary" />
-    <Button variant="secondary" />
-    <Button variant="success" />
-    <Button variant="danger" />
-    <Button />
-    <GlobalStyle />
-  </ThemeProvider>
+    <ThemeProvider theme={defaultTheme}>
+      <Button variant="primary" />
+      <Button variant="secondary" />
+      <Button variant="success" />
+      <Button variant="danger" />
+      <Button />
+      <GlobalStyle />
+    </ThemeProvider>
   )
 }
