29c9e60fd74740e680ede46f0649163e325386a2
LuizGVValentin1999 <59962559+LuizGVValentin1999@users.noreply.github.com>
2022-12-15 22:12:04 -0300

    Criando temas

--- src/App.tsx
+++ src/App.tsx
@@ -1,16 +1,19 @@
+import { ThemeProvider } from "styled-components";
 import { useState } from 'react'
 import { Button } from './components/Button'
+
+import { defaultTheme } from "./styles/themes/default";
 
 export function App() {
   const [count, setCount] = useState(0)
 
   return (
-  <>
+  <ThemeProvider theme={defaultTheme}>
     <Button variant="primary" />
     <Button variant="secondary" />
     <Button variant="success" />
     <Button variant="danger" />
     <Button />
-  </>
+  </ThemeProvider>
   )
 }
