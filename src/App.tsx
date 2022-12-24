7825542bc140185f2aa9ded9a2dff1196b407b4b
LuizGVValentin1999 <59962559+LuizGVValentin1999@users.noreply.github.com>
2022-12-24 19:09:34 -0300

    Contexto entre rotas

--- src/App.tsx
+++ src/App.tsx
@@ -2,15 +2,19 @@ import { ThemeProvider } from 'styled-components'
 import { BrowserRouter } from 'react-router-dom'
 import { Router } from './Router'
 
-import { defaultTheme } from './styles/themes/default'
 import { GlobalStyle } from './styles/global'
+import { defaultTheme } from './styles/themes/default'
+import { CyclesContextProvider } from './contexts/CyclesContext'
 
 export function App() {
   return (
     <ThemeProvider theme={defaultTheme}>
       <BrowserRouter>
-        <Router />
+        <CyclesContextProvider>
+          <Router />
+        </CyclesContextProvider>
       </BrowserRouter>
+
       <GlobalStyle />
     </ThemeProvider>
   )
--- src/pages/Home/Components/Countdown/index.tsx
+++ src/pages/Home/Components/Countdown/index.tsx
@@ -1,6 +1,6 @@
 import { differenceInSeconds } from 'date-fns'
 import { useContext, useEffect } from 'react'
-import { CyclesContext } from '../..'
+import { CyclesContext } from '../../../../contexts/CyclesContext'
 import { CountdownContainer, Separator } from './styles'
 
 export function Countdown() {
@@ -11,9 +11,7 @@ export function Countdown() {
     amountSecondsPassed,
     setSecondsPassed,
   } = useContext(CyclesContext)
-
   const totalSeconds = activeCycle ? activeCycle.minutesAmount * 60 : 0
-
   useEffect(() => {
     let interval: number
     if (activeCycle) {
@@ -24,7 +22,6 @@ export function Countdown() {
         )
         if (secondsDifference >= totalSeconds) {
           markCurrentCycleAsFinished()
-
           setSecondsPassed(totalSeconds)
           clearInterval(interval)
         } else {
@@ -32,7 +29,6 @@ export function Countdown() {
         }
       }, 1000)
     }
-
     return () => {
       clearInterval(interval)
     }
@@ -43,9 +39,7 @@ export function Countdown() {
     setSecondsPassed,
     markCurrentCycleAsFinished,
   ])
-
   const currentSeconds = activeCycle ? totalSeconds - amountSecondsPassed : 0
-
   const minutesAmount = Math.floor(currentSeconds / 60)
   const secondsAmount = currentSeconds % 60
   const minutes = String(minutesAmount).padStart(2, '0')
